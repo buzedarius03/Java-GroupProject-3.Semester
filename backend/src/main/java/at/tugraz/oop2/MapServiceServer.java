7ca85c890cebffac4132621c8c169784c2d4dea2
Benedikt Kantz <benedikt@kantz.at>
2023-10-05 00:36:54 +0200

    Framework init

---
+++ backend/src/main/java/at/tugraz/oop2/MapServiceServer.java
@@ -0,0 +1,11 @@
+package at.tugraz.oop2;
+
+import java.util.logging.Logger;
+
+public class MapServiceServer {
+    private static final Logger logger = Logger.getLogger(MapServiceServer.class.getName());
+ 
+    public static void main(String[] args) {
+        logger.info("Starting backend...");
+    }
+}
---
+++ middleware/src/main/java/at/tugraz/oop2/MapApplication.java
@@ -0,0 +1,5 @@
+package at.tugraz.oop2;
+public class MapApplication {
+    public static void main(String[] args) {
+    }
+}
