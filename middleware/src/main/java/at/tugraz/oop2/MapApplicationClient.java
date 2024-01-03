bdf8946164add1b06658c578de3f6c6316ccee27
Dominik Sert <dominik.sert@student.tugraz.at>
2024-01-03 15:44:14 +0100

    RoadbyId should be working?

--- middleware/src/main/java/at/tugraz/oop2/MapApplicationClient.java
+++ middleware/src/main/java/at/tugraz/oop2/MapApplicationClient.java
@@ -2,9 +2,19 @@ package at.tugraz.oop2;
 
 import java.util.Map;
 
+import at.tugraz.oop2.MapServiceGrpc;
+import at.tugraz.oop2.Mapservice.RoadbyIdRequest;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
 public class MapApplicationClient {
-    //private final MapServiceGrpc.MapServiceBlockingStub blockingStub;
+   ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8020).usePlaintext().build();
+    MapServiceGrpc.MapServiceBlockingStub stub = MapServiceGrpc.newBlockingStub(channel);
+
+    public Object getRoadbyId(int id) {
+        RoadbyIdRequest request = RoadbyIdRequest.newBuilder().setId(id).build();
+        Object road = stub.getRoadbyId(request).getField(null);
+        return road;
+    }
+
 }
