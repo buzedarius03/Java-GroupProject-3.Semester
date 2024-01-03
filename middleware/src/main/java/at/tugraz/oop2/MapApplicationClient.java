f946de90c1c5da4e6ee84f282057d3a62f04803a
Dominik Sert <dominik.sert@student.tugraz.at>
2024-01-03 20:31:26 +0100

    RoadbyId in Client as good as ready

--- middleware/src/main/java/at/tugraz/oop2/MapApplicationClient.java
+++ middleware/src/main/java/at/tugraz/oop2/MapApplicationClient.java
@@ -1,6 +1,11 @@
 package at.tugraz.oop2;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import com.google.protobuf.MapEntry;
 
 import at.tugraz.oop2.MapServiceGrpc;
 import at.tugraz.oop2.Mapservice.RoadbyIdRequest;
@@ -16,8 +21,12 @@ public class MapApplicationClient {
         String name = stub.getRoadbyId(request).getName();
         String type = stub.getRoadbyId(request).getType();
         double[][] coordinates = stub.getRoadbyId(request).getCoordinatesList().toArray(new double[0][0]);
-        return new Road();
-        
+        Map<String, String> tags = stub.getRoadbyId(request).getTagsMap();
+        long[] child_ids = stub.getRoadbyId(request).getChildIdsList().stream().mapToLong(i -> i).toArray();
+        String geom_type = stub.getRoadbyId(request).getGeomType();
+        Map<String, String> properties = stub.getRoadbyId(request).getProbertiesMap();
+        return new Road(name, id, geom_type,
+         coordinates, "name", properties, tags, type, child_ids);
     }
 
 }
