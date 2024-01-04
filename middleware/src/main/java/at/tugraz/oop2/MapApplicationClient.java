5085494c1d02caeed0191473b70a3e452880ff39
Dominik Sert <dominik.sert@student.tugraz.at>
2024-01-04 10:03:54 +0100

    Amenity-Func in Client

--- middleware/src/main/java/at/tugraz/oop2/MapApplicationClient.java
+++ middleware/src/main/java/at/tugraz/oop2/MapApplicationClient.java
@@ -23,7 +23,7 @@ public class MapApplicationClient {
     ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8020).usePlaintext().build();
     MapServiceGrpc.MapServiceBlockingStub stub = MapServiceGrpc.newBlockingStub(channel);
 
-    public Amenity getAmenity(String amenity, double[] point, double[] second_edge_point, long dist) {
+    public Amenity[] getAmenity(String amenity, double[] point, double[] second_edge_point, long dist) {
         //If dist == 0, we use bbox, otherwise we use points
         //if amenity == "", we use all amenities
         AmenityRequest request;
@@ -37,16 +37,20 @@ public class MapApplicationClient {
             request = AmenityRequest.newBuilder().setType(amenity).setBbox(Bbox.newBuilder().setTlX(point[0]).setTlY(point[1])
             .setBrX(second_edge_point[0]).setBrY(second_edge_point[1]).build()).build();
         }
-/* 
-        String name = stub.getEntitybyId(request).getName();
-        String type = stub.getEntitybyId(request).getType();
-        double[][] coordinates = stub.getEntitybyId(request).getCoordinatesList().toArray(new double[0][0]);
-        Map<String, String> tags = stub.getEntitybyId(request).getTagsMap();
-        String geom_type = stub.getEntitybyId(request).getGeomType();
-        Map<String, String> properties = stub.getEntitybyId(request).getPropertiesMap();
-        String crs_type = stub.getEntitybyId(request).getCrsType();
-        return new Amenity(name, id, geom_type,
-         coordinates, crs_type, properties, tags, type);*/
+
+        Amenity[] amenities = stub.getAmenity(request).getEntityList().stream().map(amenity1 -> {
+            String name = amenity1.getName();
+            long id = amenity1.getId();
+            String type = amenity1.getType();
+            double[][] coordinates = amenity1.getCoordinatesList().toArray(new double[0][0]);
+            Map<String, String> tags = amenity1.getTagsMap();
+            String geom_type = amenity1.getGeomType();
+            Map<String, String> properties = amenity1.getPropertiesMap();
+            String crs_type = amenity1.getCrsType();
+            return new Amenity(name, id, geom_type,
+             coordinates, crs_type, properties, tags, type);
+        }).toArray(Amenity[]::new);
+         return amenities;
     }
 
     public Amenity getAmenitybyId(long id) {
