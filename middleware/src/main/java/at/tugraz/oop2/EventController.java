81f84aa25218f0e63ef22fcbd47026c5383d2aee
Dominik Sert <dominik.sert@student.tugraz.at>
2024-01-05 19:10:48 +0100

    Pagging coorection

--- middleware/src/main/java/at/tugraz/oop2/EventController.java
+++ middleware/src/main/java/at/tugraz/oop2/EventController.java
@@ -37,9 +37,8 @@ public class EventController {
                 }
         Amenity[] amenities = client.getAmenity(amenity, point, bbox_br, point_dist);     
         //Not sure about the next three lines!!!!
-        Amenity[] amenities_taked = Arrays.copyOfRange(amenities, 0, Math.max(take, amenities.length));
-        skip = amenities.length - amenities_taked.length;
-        int total = skip + take;
+        Amenity[] amenities_taked = Arrays.copyOfRange(amenities, skip, Math.max(take, amenities.length));
+        int total = amenities.length;
             return Map.of(
                     "entries", amenities,
                     "paging", Map.of(
@@ -65,9 +64,8 @@ public class EventController {
                 double[] bbox_tl = {bbox_tl_x, bbox_tl_y};
         Road[] roads = client.getRoad(road, bbox_tl, bbox_br);
         //Not sure about the next three lines!!!!
-        Road[] roads_taked = Arrays.copyOfRange(roads, 0, Math.max(take, roads.length));
-        skip = roads.length - roads_taked.length;
-        int total = skip + take;
+        Road[] roads_taked = Arrays.copyOfRange(roads, skip, Math.max(take, roads.length));
+        int total = roads.length;
             return Map.of(
                     "entries", roads_taked,
                     "paging", Map.of(
