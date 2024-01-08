2f47dd003db6f5e411000cfacf6e3b6e53655c29
Dominik Sert <dominik.sert@student.tugraz.at>
2024-01-09 00:30:31 +0100

    Additional empty name check

--- middleware/src/main/java/at/tugraz/oop2/MapApplicationClient.java
+++ middleware/src/main/java/at/tugraz/oop2/MapApplicationClient.java
@@ -42,7 +42,7 @@ public class MapApplicationClient {
             .setBrX(second_edge_point[0]).setBrY(second_edge_point[1]).build()).build();
         }
         
-        Amenity[] amenities = stub.getAmenity(request).getEntityList().stream().filter(amenity1 -> amenity1.getName() != "").
+        Amenity[] amenities = stub.getAmenity(request).getEntityList().stream().filter(amenity1 -> amenity1.getName() != "").filter(amenity1 -> amenity1.getType() != "").
         map(amenity1 -> {
             String name = amenity1.getName();
             long id = amenity1.getId();
@@ -76,7 +76,7 @@ public class MapApplicationClient {
 
         request = RoadRequest.newBuilder().setType(road).setBbox(Bbox.newBuilder().setTlX(point[0]).setTlY(point[1])
         .setBrX(second_edge_point[0]).setBrY(second_edge_point[1]).build()).build();
-        Road[] roads = stub.getRoad(request).getEntityList().stream().filter(road1 -> road1.getName() != "").
+        Road[] roads = stub.getRoad(request).getEntityList().stream().filter(road1 -> road1.getName() != "").filter(road1 -> road1.getType() != "").
         map(road1 -> {
             String name = road1.getName();
             long id = road1.getId();
